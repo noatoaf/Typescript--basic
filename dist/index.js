d04e01eed899b1238b32869427e00b9429b45ae0
noa toaf <noatoaf289@gmail.com>
2023-01-12 10:02:20 +0200

    ex1+ex2+ex3

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,23 @@
 "use strict";
+function getAverage(arr) {
+    let average = 0;
+    arr.forEach(Element => {
+        average = average + Element;
+    });
+    average = average / arr.length;
+    return average;
+}
+console.log(getAverage([0, 8, 9, 3]));
+function getAmountOfPositive(arr) {
+    let count = 0;
+    arr.forEach(Element => {
+        if (Element > 0)
+            count++;
+    });
+    return count;
+}
+console.log(getAmountOfPositive([0, 8, 9, 3, -2, -9]));
+function sortList(arr) {
+    let a = arr.sort();
+    return a;
+}
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+function getAverage(arr : number[]): number {
+    let average =0 ;
+    arr.forEach(Element =>{
+        average=average+ Element;
+    });
+    average=average/arr.length;
+    return average;
+
+}
+console.log(getAverage([0,8,9,3]));
+function getAmountOfPositive(arr : number[]): number {
+    let count =0 ;
+    arr.forEach(Element =>{
+        if(Element>0)
+          count++;
+    });
+    return count;
+}
+console.log(getAmountOfPositive([0,8,9,3,-2,-9]));
+function sortList(arr : number[]): number[] {
+    let a : number[]= arr.sort();
+    return a;
+}
