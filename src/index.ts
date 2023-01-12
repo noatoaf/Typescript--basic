4171d298de7a264028b172a996fd333b4eee7852
noa toaf <noatoaf289@gmail.com>
2023-01-12 19:46:01 +0200

    [200~ User@5CG0104CZW MINGW64 ~/Desktop/קורס/Typescript- basic/Typescript--basic (main) $ git checkout -b noa Switched to a new branch 'noa'
    User@5CG0104CZW MINGW64 ~/Desktop/קורס/Typescript- basic/Typescript--basic (noa)
    $ git add .
    warning: in the working copy of 'package.json', LF will be replaced by CRLF the next time Git touches it
    
    User@5CG0104CZW MINGW64 ~/Desktop/קורס/Typescript- basic/Typescript--basic (noa)
    $ git commit -m ex1
    [noa (root-commit) ac5545b] ex1
     5 files changed, 123 insertions(+)
     create mode 100644 dist/index.js
     create mode 100644 package.json
     create mode 100644 src/Untitled-1.html
     create mode 100644 src/index.ts
     create mode 100644 tsconfig.json
    
    User@5CG0104CZW MINGW64 ~/Desktop/קורס/Typescript- basic/Typescript--basic (noa)
    $ git push -u origin noa
    Enumerating objects: 8, done.
    Counting objects: 100% (8/8), done.
    Delta compression using up to 8 threads
    Compressing objects: 100% (5/5), done.
    Writing objects: 100% (8/8), 3.92 KiB | 1.96 MiB/s, done.
    Total 8 (delta 0), reused 0 (delta 0), pack-reused 0
    To https://github.com/noatoaf/Typescript--basic.git
     * [new branch]      noa -> noa
    branch 'noa' set up to track 'origin/noa'.
    
    User@5CG0104CZW MINGW64 ~/Desktop/קורס/Typescript- basic/Typescript--basic (noa)
    $ git add .
    warning: in the working copy of 'package.json', LF will be replaced by CRLF the next time Git touches it
    
    User@5CG0104CZW MINGW64 ~/Desktop/קורס/Typescript- basic/Typescript--basic (noa)
    $ git commit -m ex1+ex2+ex3
    [noa d04e01e] ex1+ex2+ex3
     4 files changed, 52 insertions(+), 3 deletions(-)
    
    User@5CG0104CZW MINGW64 ~/Desktop/קורס/Typescript- basic/Typescript--basic (noa)
    $ git push -u origin noa
    Enumerating objects: 15, done.
    Counting objects: 100% (15/15), done.
    Delta compression using up to 8 threads
    Compressing objects: 100% (7/7), done.
    Writing objects: 100% (8/8), 1.07 KiB | 545.00 KiB/s, done.
    Total 8 (delta 3), reused 0 (delta 0), pack-reused 0
    remote: Resolving deltas: 100% (3/3), completed with 2 local objects.
    To https://github.com/noatoaf/Typescript--basic.git
       ac5545b..d04e01e  noa -> noa
    branch 'noa' set up to track 'origin/noa'.
    
    User@5CG0104CZW MINGW64 ~/Desktop/קורס/Typescript- basic/Typescript--basic (noa)
    $ git add .
    warning: in the working copy of 'package.json', LF will be replaced by CRLF the next time Git touches it
    warning: in the working copy of 'package-lock.json', LF will be replaced by CRLF the next time Git touches it
    
    User@5CG0104CZW MINGW64 ~/Desktop/קורס/Typescript- basic/Typescript--basic (noa)
    $ git commit -m new
    [noa d38c87b] new
     5 files changed, 70 insertions(+), 4 deletions(-)
     create mode 100644 .gitignore
     create mode 100644 package-lock.json
     create mode 100644 src/Changer.ts
    
    User@5CG0104CZW MINGW64 ~/Desktop/קורס/Typescript- basic/Typescript--basic (noa)
    $ git push -u origin noa
    Enumerating objects: 12, done.
    Counting objects: 100% (12/12), done.
    Delta compression using up to 8 threads
    Compressing objects: 100% (6/6), done.
    Writing objects: 100% (8/8), 1.35 KiB | 691.00 KiB/s, done.
    Total 8 (delta 1), reused 0 (delta 0), pack-reused 0
    remote: Resolving deltas: 100% (1/1), completed with 1 local object.
    To https://github.com/noatoaf/Typescript--basic.git
       d04e01e..d38c87b  noa -> noa
    branch 'noa' set up to track 'origin/noa'.
    
    User@5CG0104CZW MINGW64 ~/Desktop/קורס/Typescript- basic/Typescript--basic (noa)
    $ git add .
    
    User@5CG0104CZW MINGW64 ~/Desktop/קורס/Typescript- basic/Typescript--basic (noa)
    $ git commit -m

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-function getAverage(arr : number[]): number {
+const getAverage= ((arr : number[]): number =>{
     let average =0 ;
     arr.forEach(Element =>{
         average=average+ Element;
@@ -6,18 +6,18 @@ function getAverage(arr : number[]): number {
     average=average/arr.length;
     return average;
 
-}
+});
 console.log(getAverage([0,8,9,3]));
-function getAmountOfPositive(arr : number[]): number {
+const getAmountOfPositive= ((arr : number[]): number=> {
     let count =0 ;
     arr.forEach(Element =>{
         if(Element>0)
           count++;
     });
     return count;
-}
+});
 console.log(getAmountOfPositive([0,8,9,3,-2,-9]));
-function sortList(arr : number[]): number[] {
+const sortList=((arr : number[]): number[] =>{
     let a : number[]= arr.sort();
     return a;
-}
+});
